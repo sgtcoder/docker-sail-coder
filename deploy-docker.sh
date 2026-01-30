f3dc33a14d9d1ef153172175f3b2448cab64408d
coder <usmccoder@gmail.com>
2026-01-30 12:26:34 -0700

    docker updates with multiple php versions

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-#docker buildx create --name multiarch --driver docker-container --use
-#./deploy-docker.sh --no-cache
+SCRIPTPATH="$(dirname "$0")"
 
 REGISTRY="sgtcoder"
 PROJECT_NAME="sail-coder"
-PROJECT_TAG="8.3"
+PROJECT_TAGS=("8.3" "8.4")
+PROJECT_LATEST="8.3"
 
-docker buildx build --push --platform linux/amd64,linux/arm64 --build-arg WWWGROUP=1000 $1 -t $REGISTRY/$PROJECT_NAME:$PROJECT_TAG -t $REGISTRY/$PROJECT_NAME:latest . || exit 1
+for PROJECT_TAG in "${PROJECT_TAGS[@]}"; do
+    echo "Building and pushing $REGISTRY/$PROJECT_NAME:$PROJECT_TAG..."
+
+    cd $SCRIPTPATH/runtimes/$PROJECT_TAG
+
+    if [ "$PROJECT_TAG" = "$PROJECT_LATEST" ]; then
+        docker buildx build --push --platform linux/amd64,linux/arm64 --build-arg WWWGROUP=1000 $1 -t $REGISTRY/$PROJECT_NAME:$PROJECT_TAG -t $REGISTRY/$PROJECT_NAME:latest . || exit 1
+    else
+        docker buildx build --push --platform linux/amd64,linux/arm64 --build-arg WWWGROUP=1000 $1 -t $REGISTRY/$PROJECT_NAME:$PROJECT_TAG . || exit 1
+    fi
+done
 
 #docker push --all-tags $REGISTRY/$PROJECT_NAME
